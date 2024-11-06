efdc1d615bac0f098f04f2804d4bc05e078eb97d
Freddy Boulton <alfonsoboulton@gmail.com>
2024-11-06 02:11:42 -0500

    new gradio demo (#27)

--- webui/omni_gradio.py
+++ webui/omni_gradio.py
@@ -1,11 +1,13 @@
 """A simple web interactive chat demo based on gradio."""
 
 import os
-import time
 import gradio as gr
+from gradio_webrtc import WebRTC, AdditionalOutputs, ReplyOnPause
 import base64
 import numpy as np
 import requests
+import io
+from pydub import AudioSegment
 
 
 API_URL = os.getenv("API_URL", None)
@@ -22,54 +24,106 @@ OUT_RATE = 24000
 OUT_CHANNELS = 1
 
 
-def process_audio(audio):
-    filepath = audio
-    print(f"filepath: {filepath}")
-    if filepath is None:
-        return
+# Only needed if deploying on cloud provider
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 
-    cnt = 0
+if account_sid and auth_token:
+    from twilio.rest import Client
+    client = Client(account_sid, auth_token)
+
+    token = client.tokens.create()
+
+    rtc_configuration = {
+        "iceServers": token.ice_servers,
+        "iceTransportPolicy": "relay",
+    }
+else:
+    rtc_configuration = None
+
+    
+def response(audio: tuple[int, np.ndarray], conversation: list[dict], img: str | None):
+
+    sampling_rate, audio_np = audio
+    audio_np = audio_np.squeeze()
+
+    audio_buffer = io.BytesIO()
+    segment = AudioSegment(
+        audio_np.tobytes(),
+        frame_rate=sampling_rate,
+        sample_width=audio_np.dtype.itemsize,
+        channels=1,
+    )
+
+    segment.export(audio_buffer, format="wav")
+    conversation.append({"role": "user", "content": gr.Audio((sampling_rate, audio_np))})
+    conversation.append({"role": "assistant", "content": ""})
+
+    base64_encoded = str(base64.b64encode(audio_buffer.getvalue()), encoding="utf-8")
     if API_URL is not None:
-        with open(filepath, "rb") as f:
-            data = f.read()
-            base64_encoded = str(base64.b64encode(data), encoding="utf-8")
-            files = {"audio": base64_encoded}
-            tik = time.time()
-            with requests.post(API_URL, json=files, stream=True) as response:
-                try:
-                    for chunk in response.iter_content(chunk_size=OUT_CHUNK):
-                        if chunk:
-                            # Convert chunk to numpy array
-                            if cnt == 0:
-                                print(f"first chunk time cost: {time.time() - tik:.3f}")
-                            cnt += 1
-                            audio_data = np.frombuffer(chunk, dtype=np.int16)
-                            audio_data = audio_data.reshape(-1, OUT_CHANNELS)
-                            yield OUT_RATE, audio_data.astype(np.int16)
-
-                except Exception as e:
-                    print(f"error: {e}")
-    else:
-        tik = time.time()
-        for chunk in omni_client.run_AT_batch_stream(filepath):
-            # Convert chunk to numpy array
-            if cnt == 0:
-                print(f"first chunk time cost: {time.time() - tik:.3f}")
-            cnt += 1
-            audio_data = np.frombuffer(chunk, dtype=np.int16)
-            audio_data = audio_data.reshape(-1, OUT_CHANNELS)
-            yield OUT_RATE, audio_data.astype(np.int16)
-
+        output_audio_bytes = b""
+        files = {"audio": base64_encoded}
+        if img is not None:
+            files["image"] = str(base64.b64encode(open(img, "rb").read()), encoding="utf-8")
+        print("sending request to server")
+        resp_text = ""
+        with requests.post(API_URL, json=files, stream=True) as response:
+            try:
+                buffer = b''
+                for chunk in response.iter_content(chunk_size=2048):
+                    buffer += chunk
+                    while b'\r\n--frame\r\n' in buffer:
+                        frame, buffer = buffer.split(b'\r\n--frame\r\n', 1)
+                        if b'Content-Type: audio/wav' in frame:
+                            audio_data = frame.split(b'\r\n\r\n', 1)[1]
+                            # audio_data = base64.b64decode(audio_data)
+                            output_audio_bytes += audio_data
+                            audio_array = np.frombuffer(audio_data, dtype=np.int16).reshape(1, -1)
+                            yield (OUT_RATE, audio_array, "mono")
+                        elif b'Content-Type: text/plain' in frame:
+                            text_data = frame.split(b'\r\n\r\n', 1)[1].decode()
+                            resp_text += text_data
+                            if len(text_data) > 0:
+                                conversation[-1]["content"] = resp_text
+                                yield AdditionalOutputs(conversation)
+            except Exception as e:
+               raise Exception(f"Error during audio streaming: {e}") from e
+            
 
 def main(port=None):
 
-    demo = gr.Interface(
-        process_audio,
-        inputs=gr.Audio(type="filepath", label="Microphone"),
-        outputs=[gr.Audio(label="Response", streaming=True, autoplay=True)],
-        title="Chat Mini-Omni Demo",
-        live=True,
-    )
+    with gr.Blocks() as demo:
+        gr.HTML(
+            """
+        <h1 style='text-align: center'>
+        Mini-Omni-2 Chat (Powered by WebRTC ⚡️)
+        </h1>
+        """
+        )
+        with gr.Row():
+            with gr.Column():
+                with gr.Row():
+                    with gr.Column():
+                        audio = WebRTC(
+                            label="Stream",
+                            rtc_configuration=rtc_configuration,
+                            mode="send-receive",
+                            modality="audio",
+                        )
+                    with gr.Column():
+                        img = gr.Image(label="Image", type="filepath")
+            with gr.Column():
+                conversation = gr.Chatbot(label="Conversation", type="messages")
+            
+            audio.stream(
+                fn=ReplyOnPause(
+                    response, output_sample_rate=OUT_RATE, output_frame_size=480
+                ),
+                inputs=[audio, conversation, img],
+                outputs=[audio],
+                time_limit=90,
+            )
+            audio.on_additional_outputs(lambda c: c, outputs=[conversation])
     if port is not None:
         demo.queue().launch(share=False, server_name="0.0.0.0", server_port=port)
     else:
